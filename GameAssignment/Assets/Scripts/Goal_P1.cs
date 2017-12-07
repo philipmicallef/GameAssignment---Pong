cec5e1ba3988be29cff7f5d331a4ab626a66adef
philipmicallef <32672929+philipmicallef@users.noreply.github.com>
2017-12-07 09:55:19 +0100

    Levels Working

--- GameAssignment/Assets/Scripts/Goal_P1.cs
+++ GameAssignment/Assets/Scripts/Goal_P1.cs
@@ -1,38 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Goal_P1 : MonoBehaviour {
 
 
 	private   LevelManager   levelManager;
 
-    int p1_score = 0;
+
+    private int p1_score = 0;
+
+	public Text p1_score_text;
+
+
+
+
+
 
 		void   OnTriggerEnter2D(Collider2D   collider) 
         {
             print("Collision with " + gameObject.name);
 		}
-
-
+		
 		//this   is   triggered   when   a   collision happens
 		void   OnCollisionEnter2D(Collision2D   collision)   
 		{
 			print("Score !!");
             p1_score++;
+			SetCountText ();
             
+			if (SceneManager.GetActiveScene ().name == "Level_1" && p1_score == 3) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+			}
+
+			if (SceneManager.GetActiveScene ().name == "Level_2" && p1_score == 3) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+			}
+
+			if (SceneManager.GetActiveScene ().name == "Level_3" && p1_score == 3) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+			}
 		}
 
 		// Use this for initialization
 		void Start () {
-			
+			levelManager = GameObject.FindObjectOfType<LevelManager>();
+
+		}
+
+
+		void SetCountText()
+		{
+			p1_score_text.text = "Player 1 Score: " + p1_score.ToString ();
 		}
 		
 		// Update is called once per frame
 		void Update () {
-
-	        if(p1_score == 3){
-	            levelManager.LoadLevel("Level_2");
-	        }
 		}
 }
---
+++ GameAssignment/Assets/Scripts/Goal_P2.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Goal_P2 : MonoBehaviour {
+
+
+	private   LevelManager   levelManager;
+
+
+	private int p2_score = 0;
+
+	public Text p2_score_text;
+
+
+	void   OnTriggerEnter2D(Collider2D   collider) 
+	{
+		print("Collision with " + gameObject.name);
+	}
+
+	//this   is   triggered   when   a   collision happens
+	void   OnCollisionEnter2D(Collision2D   collision)   
+	{
+		
+		print("Score !!");
+		p2_score++;
+		SetCountText ();
+
+		if (SceneManager.GetActiveScene ().name == "Level_1" && p2_score == 3) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+		}
+
+		if (SceneManager.GetActiveScene ().name == "Level_2" && p2_score == 3) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+		}
+
+		if (SceneManager.GetActiveScene ().name == "Level_3" && p2_score == 3) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+		}
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+
+	}
+
+
+	void SetCountText()
+	{
+		p2_score_text.text = "Player 2 Score: " + p2_score.ToString ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
